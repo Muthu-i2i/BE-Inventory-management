5b8b954b84120034620db61862e15ef0f35530f3
Muthukumar-Ramasamy <rmuthukumar306@gmail.com>
2025-08-04 00:21:31 +0530

    cors fix

--- src/server.ts
+++ src/server.ts
@@ -15,10 +15,8 @@ const app = express();
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: '*',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
 }));
 app.use(express.json());
 
